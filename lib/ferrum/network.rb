0b64fedfce8c5f121bf1a7af81148c7a465f8dc7
Francis Beaudoin <francis.beaudoin@shopify.com>
2023-02-14 16:04:35 -0500

    Fix

--- lib/ferrum/network.rb
+++ lib/ferrum/network.rb
@@ -385,7 +385,7 @@ module Ferrum
 
     def subscribe_loading_finished
       @page.on("Network.loadingFinished") do |params|
-        exchange = select(params["requestId"]).last&.response
+        response = select(params["requestId"]).last&.response
 
         if response
           response.loaded = true
